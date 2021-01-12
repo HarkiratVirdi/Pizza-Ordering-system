d2eed8152a277b8abcd7852b2a81c346ab104550
Harkirat Singh Virdi <57691094+HarkiratVirdi@users.noreply.github.com>
2021-01-12 14:31:00 -0500

    Update conn.php

--- db_connect/conn.php
+++ db_connect/conn.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $username = "root";
-$password = "Jetairways1";
+$password = "YourFavouritePassword";
 $database = "testDB";
 
 $conn = mysqli_connect($host, $username, $password, $database);
